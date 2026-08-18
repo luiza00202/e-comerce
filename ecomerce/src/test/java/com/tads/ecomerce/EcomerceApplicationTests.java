2cae2b584aabe0e617133825d73f2e713092e9b8
luiza00202 <luizinha.melissinha@gmail.com>
2026-08-17 21:11:02 -0400

    Projeto Criado

---
+++ ecomerce/src/test/java/com/tads/ecomerce/EcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tads.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
